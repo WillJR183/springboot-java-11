61f4947b8c08f6c1f6f7c886e3c36dc628f71635
William Marques <junior.stuart34@gmail.com>
2022-03-06 21:39:05 -0400

    User delete

--- src/main/java/com/coursejava/jpahibernate/services/UserService.java
+++ src/main/java/com/coursejava/jpahibernate/services/UserService.java
@@ -19,6 +19,10 @@ public class UserService {
 		return userRepository.save(obj);
 	}
 	
+	public void delete(Long id) {
+		userRepository.deleteById(id);;
+	}
+	
 	public List<User> findAll(){
 		return userRepository.findAll();
 	}
