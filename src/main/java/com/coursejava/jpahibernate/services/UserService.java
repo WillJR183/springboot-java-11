619cd730c66de683b263c8e8c251210dad7202a1
William Marques <junior.stuart34@gmail.com>
2022-03-07 15:39:09 -0400

    Exception handling delete

--- src/main/java/com/coursejava/jpahibernate/services/UserService.java
+++ src/main/java/com/coursejava/jpahibernate/services/UserService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.coursejava.jpahibernate.entities.User;
 import com.coursejava.jpahibernate.repositories.UserRepository;
+import com.coursejava.jpahibernate.services.exceptions.DatabaseException;
 import com.coursejava.jpahibernate.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -21,7 +24,15 @@ public class UserService {
 	}
 	
 	public void delete(Long id) {
-		userRepository.deleteById(id);;
+		try {
+			userRepository.deleteById(id);
+		}
+		catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		}
+		catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
 	}
 	
 	public User update(Long id, User obj) {
---
+++ src/main/java/com/coursejava/jpahibernate/services/exceptions/DatabaseException.java
@@ -0,0 +1,11 @@
+package com.coursejava.jpahibernate.services.exceptions;
+
+public class DatabaseException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+	
+	public DatabaseException(String msg) {
+		super(msg);
+	}
+
+}
