562a5aaf6f10b1b300840ccc9152f25d348b5867
William Marques <junior.stuart34@gmail.com>
2022-03-06 21:48:57 -0400

    User update

--- src/main/java/com/coursejava/jpahibernate/services/UserService.java
+++ src/main/java/com/coursejava/jpahibernate/services/UserService.java
@@ -23,6 +23,12 @@ public class UserService {
 		userRepository.deleteById(id);;
 	}
 	
+	public User update(Long id, User obj) {
+		User entity = userRepository.getById(id);
+		updateData(entity, obj);
+		return userRepository.save(entity);
+	}
+	
 	public List<User> findAll(){
 		return userRepository.findAll();
 	}
@@ -30,6 +36,12 @@ public class UserService {
 	public User findById(Long id) {
 		Optional<User> obj =  userRepository.findById(id);
 		return obj.get();
+	}
+	
+	private void updateData(User entity, User obj) {
+		entity.setName(obj.getName());
+		entity.setEmail(obj.getEmail());
+		entity.setPhone(obj.getPhone());
 	}
 
 }
